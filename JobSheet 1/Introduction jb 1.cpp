5d636b0cbd14a0522614d352dd86005e1530bb24
nanaux <144121287+nanaux@users.noreply.github.com>
2024-05-28 10:02:21 +0700

    Delete JobSheet 1 directory

--- JobSheet 1/Introduction jb 1.cpp
+++
@@ -1,8 +0,0 @@
-//Created by 23343068_Hanna Fadilah
-
-#include <stdio.h>
-int main()
-{
-	printf("\nWelcome to the world of C"); //prints the message on the screen
-	return 0; //returns a value 0 to the operating system
-}
