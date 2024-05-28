9f948d4afcccd248c949b5fabd5aeb6ad76c3486
nanaux <144121287+nanaux@users.noreply.github.com>
2024-05-28 10:03:51 +0700

    Add files via upload

---
+++ JobSheet 1/Latihan/Introduction jb 1.cpp
@@ -0,0 +1,8 @@
+//Created by 23343068_Hanna Fadilah
+
+#include <stdio.h>
+int main()
+{
+	printf("\nWelcome to the world of C"); //prints the message on the screen
+	return 0; //returns a value 0 to the operating system
+}
